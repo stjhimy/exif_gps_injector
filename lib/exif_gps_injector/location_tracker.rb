9e99c3a9485e665f578f213417df94ed87bfbf69
stjhimy <stjhimy@gmail.com>
2015-12-26 23:18:00 -0200

    Add locate_at to location_tracker

--- lib/exif_gps_injector/location_tracker.rb
+++ lib/exif_gps_injector/location_tracker.rb
@@ -7,5 +7,16 @@ module ExifGpsInjector
               .reduce({}, :merge)
               .sort.to_h
     end
+    def list
+      @list
+    end
+
+    def self.instance
+    end
+
+    def self.locate_at(date)
+      instance.list.select{|key, value| key <= date && key >= date - 3.minutes }.to_a.last.last rescue nil ||
+        instance.list.select{|key, value| key >= date && key <= date + 3.minutes }.to_a.first.last rescue nil
+    end
   end
 end
