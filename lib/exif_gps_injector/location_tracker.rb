c901132c75f74bfef9f06deccd008bac468d806b
stjhimy <stjhimy@gmail.com>
2015-12-27 22:36:02 -0200

    Remove class methods

--- lib/exif_gps_injector/location_tracker.rb
+++ lib/exif_gps_injector/location_tracker.rb
@@ -7,16 +7,10 @@ module ExifGpsInjector
               .reduce({}, :merge)
               .sort.to_h
     end
-    def list
-      @list
-    end
 
-    def self.instance
-    end
-
-    def self.locate_at(date)
-      instance.list.select{|key, value| key <= date && key >= date - 3.minutes }.to_a.last.last rescue nil ||
-        instance.list.select{|key, value| key >= date && key <= date + 3.minutes }.to_a.first.last rescue nil
+    def locate_at(date)
+      @list.select{ |key, value| key <= date && key >= date - 3.minutes }.to_a.last.last rescue nil ||
+        @list.select{ |key, value| key >= date && key <= date + 3.minutes }.to_a.first.last rescue nil
     end
   end
 end
