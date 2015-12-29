9a370b682b53455a60abdd9193afa895e314bba9
stjhimy <stjhimy@gmail.com>
2015-12-29 15:27:35 -0200

    Finish locate_at and max_distance_time

--- lib/exif_gps_injector/location_tracker.rb
+++ lib/exif_gps_injector/location_tracker.rb
@@ -2,17 +2,27 @@ module ExifGpsInjector
   # LocationTracker
   class LocationTracker
     attr_reader :list
+    attr_accessor :max_distance_time
 
-    def initialize(file)
-      @list = Nokogiri::XML(File.open(file)).search('when')
-              .map { |e| { (Time.parse(e.text).utc - 2.hours) => e.next_element.text } }
-              .reduce({}, :merge)
-              .sort.to_h
+    def initialize(file, options = {})
+      @max_distance_time = options.delete(:max_distance_time) || 2.minutes
+      @kml = File.open(file)
+      @list = extract_list_from_kml
     end
 
     def locate_at(date)
-      @list.select { |key, _value| key <= date && key >= date - 3.minutes }.to_a.last.last ||
-        @list.select { |key, _value| key >= date && key <= date + 3.minutes }.to_a.first.last
+      date = DateTime.parse(date).utc
+      @list.select { |key, _value| key <= date && key >= date - @max_distance_time }.to_a.last.try(:last) ||
+        @list.select { |key, _value| key >= date && key <= date + @max_distance_time }.to_a.first.try(:last)
+    end
+
+    private
+
+    def extract_list_from_kml
+      Nokogiri::XML(@kml).search('when')
+        .map { |e| { (DateTime.parse(e.text).utc) => e.next_element.text } }
+        .reduce({}, :merge)
+        .sort.to_h
     end
   end
 end
--- spec/exif_gps_injector/location_tracker_spec.rb
+++ spec/exif_gps_injector/location_tracker_spec.rb
@@ -1,10 +1,23 @@
 require 'spec_helper'
 
 describe ExifGpsInjector::LocationTracker do
+  let(:kml) { File.join(File.dirname(__FILE__), '../fixtures/history.kml') }
+
   it 'parse a list of locations based on kml file' do
-    file = File.join(File.dirname(__FILE__), '../fixtures/history.kml')
-    tracker = ExifGpsInjector::LocationTracker.new(file)
+    tracker = ExifGpsInjector::LocationTracker.new(kml)
     expect(tracker.list).to_not be_nil
     expect(tracker.list).to_not eq({})
+  end
+
+  describe 'locate_at' do
+    it 'return gps coordinates' do
+      tracker = ExifGpsInjector::LocationTracker.new(kml)
+      expect(tracker.locate_at('2015-12-27T02:00:35+00:00')).to eq('-48.000 -21.000 0')
+    end
+
+    it 'return nil when no match is found' do
+      tracker = ExifGpsInjector::LocationTracker.new(kml)
+      expect(tracker.locate_at('2015-01-01')).to eq(nil)
+    end
   end
 end
